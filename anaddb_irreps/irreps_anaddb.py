b0aa580262585c0a017843fdb2dfbec09ee4a5cd
Matthieu Verstraete <matthieu.jean.verstraete@gmail.com>
2023-06-14 18:00:05 +0200

    added IR Raman recognition to beginning of run

--- anaddb_irreps/irreps_anaddb.py
+++ anaddb_irreps/irreps_anaddb.py
@@ -37,17 +37,18 @@ class IrRepsEigen(IrReps):
         self._symmetry_dataset = Symmetry(self._primitive,
                                           symprec=self._symprec).get_dataset()
 
-        # if not self._is_primitive_cell():
-        #    print('')
-        #    print("Non-primitve cell is used.")
-        #    print("Your unit cell may be transformed to a primitive cell "
-        #          "by PRIMITIVE_AXIS tag.")
-        #    return False
+#        if not self._is_primitive_cell():
+#            print('')
+#            print("Non-primitve cell is used.")
+#            print("Your unit cell may be transformed to a primitive cell "
+#                  "by PRIMITIVE_AXIS tag.")
+#            return False
         if not is_primitive_cell(self._symmetry_dataset["rotations"]):
             raise RuntimeError(
                 "Non-primitve cell is used. Your unit cell may be transformed to "
                 "a primitive cell by PRIMITIVE_AXIS tag."
             )
+
 
         (self._rotations_at_q,
          self._translations_at_q) = self._get_rotations_at_q()
@@ -67,8 +68,14 @@ class IrRepsEigen(IrReps):
         if (self._pointgroup_symbol in character_table.keys()
                 and character_table[self._pointgroup_symbol] is not None):
             self._rotation_symbols = self._get_rotation_symbols()
+            #print (" char tab ", self._character_table)
+
             if (abs(self._q) < self._symprec).all() and self._rotation_symbols:
                 self._ir_labels = self._get_ir_labels()
+                self._RamanIR_labels = self._get_infrared_raman()
+                IR_labels, Ram_labels = self._RamanIR_labels
+                print ("IR labels ", IR_labels)
+                print ("Ram labels ", Ram_labels)
             elif (abs(self._q) < self._symprec).all():
                 if self._log_level > 0:
                     print("Database for this point group is not preprared.")
@@ -85,6 +92,130 @@ class IrRepsEigen(IrReps):
                                        cutoff=self._degeneracy_tolerance)
         return deg_sets
 
+    def _get_infrared_raman(self):
+        """once we have irreps and characters, use them and symops to find the IR and Raman active irreps
+        http://symmetry.jacobs-university.de/cgi-bin/group.cgi?group=606&option=4
+        """
+# make symops in cartesian space
+        rprim = self._primitive.cell  #np.identity(3)
+        gprim = np.linalg.inv(rprim).T
+        #print (" R G ", rprim, gprim)
+        #print ("rot ", self._rotations_at_q)
+
+# make cartesian symop matrices for each operation in each class
+# then get characters for IR and Raman reducible representations
+        nclass = len(self._character_table['rotation_list'])
+        self._cartesian_rotations_at_q = np.zeros([nclass,96,3,3])
+        degenclass = np.zeros(nclass)
+        characters_xyz = np.zeros(nclass)
+        chardegen_xyz = np.zeros(nclass)
+        characters_x2 = np.zeros(nclass)
+        chardegen_x2 = np.zeros(nclass)
+        iclass = 0
+        for opclass in self._character_table['mapping_table'].keys():
+          degenclass[iclass] = len(self._character_table['mapping_table'][opclass])
+          iop = 0
+          for symop in  np.array(self._character_table['mapping_table'][opclass][:]):
+            #print ("rotred ", symop)
+            self._cartesian_rotations_at_q[iclass][iop] = np.dot(rprim, np.dot(symop, gprim.T))
+            #print (" rotcart ", self._cartesian_rotations_at_q[isym])
+
+            #m = self._cartesian_rotations_at_q[iclass][iop]
+            #character_x2_iop =  np.matrix.trace(np.block([[m*m[0,0], m*m[0,1], m*m[0,2]],\
+            #                                              [m*m[1,0], m*m[1,1], m*m[1,2]],\
+            #                                              [m*m[2,0], m*m[2,1], m*m[2,2]]]))
+            #print ("class ", opclass, " op ", iop, " character ", character_x2_iop)
+
+            iop += 1 
+
+          m = self._cartesian_rotations_at_q[iclass][0]
+# get representation characters for x,y,z functions
+          characters_xyz[iclass] = np.matrix.trace(m)
+
+# get representation characters for quadratic functions
+# line below is in x2 xy y2 xz yz z2 format
+          bigmat = np.zeros([6,6])
+          ibig = 0
+          for ixyz in range(3):
+            for ixyz_prime in range(ixyz+1):
+              outprod = np.ndarray.flatten(np.outer(m[:,ixyz],m[:,ixyz_prime]))
+              bigmat[ibig,:] = [outprod[0], \
+                 outprod[1]+outprod[3], outprod[4], \
+                 outprod[2]+outprod[6], outprod[5]+outprod[7], outprod[8]]
+                
+              ibig += 1
+          #print (" class ", iclass, opclass, " x2 matrix", bigmat)
+
+          characters_x2[iclass] = np.matrix.trace(bigmat)
+          #print (" class ", iclass, "x2 charac ", characters_x2[iclass])
+
+          chardegen_xyz[iclass] = characters_xyz[iclass]*degenclass[iclass]
+          chardegen_x2[iclass]  = characters_x2[iclass]*degenclass[iclass]
+          #print ("xyz charac ", characters_xyz[iclass], " degen ", self._degenclass[iclass])
+          iclass += 1
+
+# now we have red representations, project them into irreps
+        #print ("irrep  characters g = ", self._g)
+        xyzlabels = ['x','y','z']
+        x2labels = ['x^2', 'xy', 'y^2', 'xz', 'yz', 'z^2']
+        IR_dict={'x':None, 'y':None, 'z':None}
+        Raman_dict={'x^2':[],'xy':[],'y^2':[],'xz':[],'yz':[],'z^2':[]}
+
+# loop over irreducible representations
+        i_ir = 0
+        for irreplabel in self._character_table['character_table'].keys():
+          # characters
+          irr_char = self._character_table['character_table'][irreplabel]
+          # l_n dimention of current irreps
+          len_irr = irr_char[0]
+          # number of ir modes here
+          n_ir = int(np.dot(irr_char,chardegen_xyz)/self._g)
+          # number of Raman modes here
+          n_ram = int(np.dot(irr_char,chardegen_x2)/self._g)
+          #print (irreplabel, " nir ", n_ir, " nram ", n_ram, " irchar ", irr_char)
+
+# find eigenvectors: are x y or z isolated in representation?
+# IR
+          for ixyz in range(3):
+            xyzvec = np.zeros(3)
+            for iclass in range(len(self._character_table['mapping_table'].keys())):
+              opclass = list(self._character_table['mapping_table'].keys())[iclass]
+              degenclass = len(self._character_table['mapping_table'][opclass][:])
+              for iop in range(degenclass):
+                xyzvec += irr_char[iclass] * self._cartesian_rotations_at_q[iclass][iop][ixyz,:]
+            xyzvec *= len_irr/self._g
+            if np.linalg.norm(xyzvec) > 1.e-6:
+              IR_dict[xyzlabels[ixyz]] = irreplabel
+
+# find the irreps which contain each of the quadratic functions (not full linear combination basis functions, but still)
+# Raman
+          ibig = 0
+          bigvec = np.zeros(6)
+          for ixyz in range(3):
+            for ixyz_prime in range(ixyz+1):
+              x2vec = np.zeros(6)
+# loop over all operations
+              for iclass in range(len(self._character_table['mapping_table'].keys())):
+                opclass = list(self._character_table['mapping_table'].keys())[iclass]
+                degenclass = len(self._character_table['mapping_table'][opclass][:])
+                for iop in range(degenclass):
+                  m = self._cartesian_rotations_at_q[iclass][iop]
+                  outprod = np.ndarray.flatten(np.outer(m[:,ixyz],m[:,ixyz_prime]))
+                  bigvec = np.array([outprod[0], \
+                     outprod[1]+outprod[3], outprod[4], \
+                     outprod[2]+outprod[6], outprod[5]+outprod[7], outprod[8]])
+                  x2vec  += irr_char[iclass] * bigvec
+
+              x2vec *= len_irr/self._g
+              if np.linalg.norm(x2vec) > 1.e-6:
+                #print (x2labels[ibig], " belongs to ", irreplabel , " norm ", np.linalg.norm(x2vec))
+                Raman_dict[x2labels[ibig]].append(irreplabel)
+
+              ibig += 1
+# loop over irreps
+          i_ir += 1
+
+        return IR_dict, Raman_dict
 
 class IrRepsAnaddb(IrRepsEigen):
     def __init__(self,
